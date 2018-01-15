68f2615b634687aa6c6551c38adf3d1cb2177fbe
Kelly <captainkelly6@gmail.com>
2018-01-15 09:38:45 -0700

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -5,6 +5,16 @@ console.log("Sup World");
 }
 
 var picture = "";
+(function(){
+  var app = angular.modul('test', []);
+  app.controller('TestController', function(){
+    this.details
+  });
+  var details = {
+    name: 'Testing 123'
+  }
+  
+})();
 
 
 my first();
