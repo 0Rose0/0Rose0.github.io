c7a9eb72ee8b6193ca6818fe3a5692adc632d6b0
Kelly <captainkelly6@gmail.com>
2018-01-24 15:46:12 -0700

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -10,10 +10,20 @@ console.log("Sup World");
     this.details = vdetails;
     this.picture = pic;
   });
-  var vdetails = {
+  var vdetails = [
+    
+    {
     name:"Testing 123",
+    testNum:1,
     succeed: true
-  }
+    },
+    
+   {
+    name:"Testing 123",
+    testNum:2,
+    succeed: false
+    }
+  ];
   var pic = "https://user-images.githubusercontent.com/16003525/33234973-ad15f752-d1ec-11e7-9224-7046c95b540a.png";
   
   function tool (inUse, design, x, y) {
