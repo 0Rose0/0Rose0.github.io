6686668f8d78169fe43c0b96e720da2fd38a1781
Kelly <captainkelly6@gmail.com>
2018-01-24 15:30:45 -0700

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -4,24 +4,27 @@ function myfirst(){
 console.log("Sup World");
 }
 
-var picture = "";
 (function(){
   var app = angular.modul('test', []);
   app.controller('TestController', function(){
     this.details
+    tihs.picture
   });
   var details = {
     name: 'Testing 123'
     succeed: true
   }
+  var picture = "https://user-images.githubusercontent.com/16003525/33234973-ad15f752-d1ec-11e7-9224-7046c95b540a.png";
   
-  var tool = {
-    activated: false,
-    sticker: "",
-    xloc: 0;
-    yloc: 0
+  function tool (inUse, design, x, y) {
+    this.activated = inUse;
+    this.sticker = design;
+    this.xloc = x;
+    this.yloc =y;
    // Names need paraethesis or not?//
   }
+  
+  var Heart = new tool(false, "heart", 0, 0);
   
 })();
 
