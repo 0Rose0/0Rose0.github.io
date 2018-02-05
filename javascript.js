e9f6ab5d0a7753439bf77d139e37c21546afb681
Kelly <captainkelly6@gmail.com>
2018-02-05 09:52:28 -0700

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -16,14 +16,14 @@ console.log("Sup World");
     name:"Testing 123",
     testNum:1,
     succeed: true,
-    price: '$3'
+    price: '3'
     },
     
    {
     name:"Testing 124",
     testNum:2,
     succeed: false,
-    price: '$5'
+    price: '5'
     }
   ];
   var pic = "https://user-images.githubusercontent.com/16003525/33234973-ad15f752-d1ec-11e7-9224-7046c95b540a.png";
