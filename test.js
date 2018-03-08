b48162979890c61cea2f328cebbee4e0ea161e07
Kelly <captainkelly6@gmail.com>
2018-03-08 16:22:33 -0700

    Update test.js

--- test.js
+++ test.js
@@ -1,44 +1,19 @@
 $(document).ready(function(){
   //JQuery(document) instead?//
 
-          var selected = '';        
+             
  // $("button").remove();
  // $(".uploadbutton").remove();
   
   
   
-  $("button").on("click", function () {
-       // Get the vertical coordinate
-    if (this.id == selected){
-      alert("Same");
-    }
-    else if (pastIcon == ''){
-      selected = this.id;
-      {
-      else{
-        var pastIcon = document.getElementById(selected);
-        pastIcon.style.display = "none";
-        selected = this.id;
-  
-      }
  
-    });
 
  
   
   $(".picture").click(function () {
       $("button").remove();
-    
-       // Get the vertical coordinate
-    alert( "Handler for .click() called." );
-var x = event.clientX;     // Get the horizontal coordinate
-var y = event.clientY; 
-    //var icon = document.getElementById(selected);
-    var icon = document.getElementById("#heartAppear");
-    icon.style.display = '';
-    icon.style.position = 'absolute';
-    icon.style.left = x + 'px';
-    icon.style.top = y + 'px';
+  
     
   });
 });
