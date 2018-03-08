d1544e9bf9da8a2e46f58d70804403dea21e7451
Kelly <captainkelly6@gmail.com>
2018-03-08 16:56:01 -0700

    Update test.js

--- test.js
+++ test.js
@@ -18,10 +18,25 @@ var y = event.clientY;
     var icon = document.getElementById("heartAppear");
     icon.style.display = '';
     icon.style.position = 'absolute';
-    icon.style.left = x + 'px';
-    icon.style.top = y + 'px';
+    icon.style.left = x;
+    icon.style.top = y ;
     
   
     
   });
 });
+
+
+ //$(".picture").on("click", function () {
+     // $("button").remove();
+   // var x = event.clientX;     // Get the horizontal coordinate
+//var y = event.clientY; 
+  //  var icon = document.getElementById("heartAppear");
+    //icon.style.display = '';
+   // icon.style.position = 'absolute';
+    //icon.style.left = x + 'px';
+    //icon.style.top = y + 'px';
+    
+  
+    
+//  });
