52456513a5497d690e945456c774607ae75a1eb1
Kelly <captainkelly6@gmail.com>
2018-03-08 16:00:32 -0700

    Update test.js

--- test.js
+++ test.js
@@ -8,9 +8,22 @@ $(document).ready(function(){
   
   
   $("button").on("click", function () {
-      
+       // Get the vertical coordinate
+    if (this.id == selected){
+      alert("Same");
+    }
+    else{
+      var pastIcon = document.getElementById(selected);
+      if (pastIcon == ''){
+         selected = this.id;
    alert(this.id);
-    
+        
+      }
+      else{
+        pastIcon.style.display = "none";
+        selected = this.id;
+   alert(this.id);
+      }
  
     });
 
