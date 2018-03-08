8943a4310226f7cbe1474765fffec03a511d0655
Kelly <captainkelly6@gmail.com>
2018-03-08 16:33:03 -0700

    Update test.js

--- test.js
+++ test.js
@@ -12,7 +12,15 @@ $(document).ready(function(){
  
   
   $(".picture").on("click", function () {
-      $("button").remove();
+     // $("button").remove();
+    var x = event.clientX;     // Get the horizontal coordinate
+var y = event.clientY; 
+    var icon = document.getElementById("#heartAppear");
+    icon.style.display = '';
+    icon.style.position = 'absolute';
+    icon.style.left = x + 'px';
+    icon.style.top = y + 'px';
+    
   
     
   });
