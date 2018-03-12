02b2d232af22fc4d3a9dcf2da6488d2b3144eb35
Kelly <captainkelly6@gmail.com>
2018-03-12 09:53:26 -0600

    Update test.js

--- test.js
+++ test.js
@@ -1,11 +1,27 @@
 $(document).ready(function(){
   //JQuery(document) instead?//
-
+var selected = '';
              
  // $("button").remove();
  // $(".uploadbutton").remove();
   
+  $("button").on("click", function () {
+    //var thisIcon = document.getElementById(this.id);
+var pastIcon = document.getElementById(selected);
+    if (this.id == selected){
+      alert("Same");
+    }
+    else if (pastIcon == ''){
+      selected = this.id;
+      {
+      else{
+        
+        pastIcon.style.display = "none";
+        selected = this.id;
   
+      }
+ 
+    });
 
  
 
@@ -17,7 +33,7 @@ $(document).ready(function(){
 //var y = event.clientY; 
      var x = event.screenX;     // Get the horizontal coordinate
 var y = event.screenY;
-    var icon = document.getElementById("heartAppear");
+    var icon = document.getElementById("selected");
     icon.style.display = '';
     icon.style.position = 'absolute';
     icon.style.left = x + 'px';
