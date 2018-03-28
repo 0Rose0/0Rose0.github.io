5b5210c5ed9c1aea057c0533aab6c147736325b6
Kelly <captainkelly6@gmail.com>
2018-03-28 09:14:44 -0600

    Update script.js

--- script.js
+++ script.js
@@ -29,7 +29,36 @@ var start = "i'm starting";
   ];
   var pic = "https://user-images.githubusercontent.com/16003525/33234973-ad15f752-d1ec-11e7-9224-7046c95b540a.png";
   
-  
+     function previewFile(){
+       var preview = document.querySelector('img'); //selects the query named img
+       var file    = document.querySelector('input[type=file]').files[0]; //sames as here
+       var reader  = new FileReader();
+       reader.onloadend = function () {
+           preview.src = reader.result;
+       }
+       if (file) {
+           reader.readAsDataURL(file); //reads the data as a URL
+       } else {
+           preview.src = "";
+       }
+    make_base();
+  }
+  previewFile();  //calls the function named previewFile()
+   
+   var canvas = document.getElementById('viewPort'),
+context = canvas.getContext('2d');
+make_base();
+function make_base()
+{
+ var preview = document.querySelector('img');
+ 
+ 
+  base_image = new Image();
+  base_image.src = preview.src;
+  base_image.onload = function(){
+    context.drawImage(base_image, 0, 0);
+  }
+}
 
 // functions
 
