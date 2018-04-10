f00830a9a5ed57864053561b7642794f40e36659
Kelly <captainkelly6@gmail.com>
2018-04-10 13:57:46 -0600

    Update script.js

--- script.js
+++ script.js
@@ -61,14 +61,11 @@ function make_base()
 }
 
 function makeGrey(){
-//var c=document.getElementById("viewPort");
-//var ctx=c.getContext("2d");
-//var img=document.getElementById("pic");
-   var c=document.getElementById("viewPort");
+var c=document.getElementById("viewPort");
 var ctx=c.getContext("2d");
-var img=document.getElementById("viewPort");
-  
-ctx.drawImage(img,0,0);
+//var img=document.getElementById("pic");
+//ctx.drawImage(img,0,0);
+
 var imgData=ctx.getImageData(0,0,c.width,c.height);
 // invert colors
 for (var i=0;i<imgData.data.length;i+=4)
